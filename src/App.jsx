77c0e3a0b9b10368f7199c2d898fd031ff3e4526
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-02-28 22:31:20 +0700

    Design: main app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
+import './sass/mainApp.scss'
 
 function App() {
 
   return (
-    <>
+    <div className='main-container'>
+      <main className='main'>
+      {/* header */}
+        <header className="main__head">
+          <h1>
+            ADVICE #117
+          </h1>
+        </header>
 
-    </>
+        <article className="main__quotes">
+          <p>
+            {`"Some quotes here"`}
+          </p>
+        </article>
+
+        <div className="main__divider">
+          <img src="../images/pattern-divider-mobile.svg" alt="divider-line" />          
+        </div>        
+      </main>
+      <div className='dice-container'>
+        <img src="../images/icon-dice.svg" alt="dice" />
+      </div>
+    </div>
   )
 }
 export default App
