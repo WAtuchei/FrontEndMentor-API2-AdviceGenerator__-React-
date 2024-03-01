bd10247bfdc068902f5d74f0e4aac44ca0eba6e6
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-03-01 23:24:52 +0700

    Feat: random quotes func

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 import './sass/mainApp.scss'
 
 function App() {
+  const [advice, setAdvie] = useState('')
+  const [quote, setQuote] = useState('')
+
+  const randomQuote = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then( res => {
+      const data = res.data.slip
+      setAdvie(data.id)
+      setQuote(data.advice)
+    })
+  }
 
   return (
     <div className='main-container'>
@@ -9,22 +22,32 @@ function App() {
       {/* header */}
         <header className="main__head">
           <h1>
-            ADVICE #117
+            { !advice ? 
+              `ADVICE GENERATOR` :
+              `ADVICE #${advice}`
+            }
           </h1>
         </header>
+
       {/* quotes block */}
         <article className="main__quotes">
           <p>
-            {`"Some quotes here"`}
+            { quote &&
+              `"${quote}"`
+            }
           </p>
         </article>
+
       {/* divide line */}
         <div className="main__divider">
           <img src="../images/pattern-divider-mobile.svg" alt="divider-line" />          
         </div>        
       </main>
       {/* dice */}
-      <div className='dice-container'>
+      <div 
+        className='dice-container'
+        onClick={randomQuote}
+      >
         <img src="../images/icon-dice.svg" alt="dice" />
       </div>
     </div>
